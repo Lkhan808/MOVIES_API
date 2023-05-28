91ceff69e627ed298e44d7f2c655e13680222ba5
Эльхан <pirigamelkhan@icloud.com>
2023-05-28 16:38:58 +0600

    d

--- djangoMOVIESAPI/prod_settings.py
+++ djangoMOVIESAPI/prod_settings.py
@@ -5,21 +5,21 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'django123-insecure-@xww&n@zay80d&3-&$_dspfz*pb+%(8oi4q4p0j!56l#l_(%*u)'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '138.197.185.196']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'movie',
-        'USER': 'postgres',
+        'USER': 'userdb',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '5432'
     }
 }
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+#STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [STATIC_DIR]
